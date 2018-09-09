571b69ea7b0ba21bb1661e283b1066a56312a3dc
Lorenzo <tognesco@libero.it>
2018-09-09 20:04:37 +0200

    Inizialised model User

--- models/user.js
+++ models/user.js
@@ -1,1 +1,12 @@
 const mongoose = require('mongoose');
+const Joi = require('joi');
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        minlength: 4,
+        maxlength: 255,
+        required: true
+    },
+    
+});
