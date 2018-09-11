2de9a363f25b51ff73a3c3ac56c76380dbe2f7f9
Lorenzo <tognesco@gmail.com>
2018-09-11 20:33:06 +0200

    Started registration, loggin

--- routes/home.js
+++ routes/home.js
@@ -12,15 +12,27 @@ router.post('/', (req, res) => {
 });
 
 router.post('/api/registerUser', async (req, res) => {
-    const user = new User(
+    /*const user = new User(
         _.pick(req.body, ['txtFirstName', 'txtLastName', 'txtEmail', 'txtPassword', 'txtConfirmPassword'])
-    );
+    );*/
+
+    const user = new User({
+        firstName: req.body.txtFirstName,
+        lastName: req.body.txtLastName,
+        email: req.body.txtEmail, 
+        password: req.body.txtPassword      
+    }); //TODO Sostituire req.body con .pick() di lodash
+
     await user.save();
     res.send('User Registered');
 });
 
-router.post('/api/loginUser', (req, res) => {
-    console.log(req.body.txtEmailLogin);
+router.post('/api/loginUser', async (req, res) => {
+    const user = await User.findOne({ email: req.body.txtEmail, password: req.body.txtEmail });
+    if(!user) {
+        res.send("The email or/and the password is/are wrong");
+    }
+    res.send('User Logged');
 });
 
 module.exports = router;
