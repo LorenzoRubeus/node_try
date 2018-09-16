8ef985ceced8bef3e1338ba7efb73163530e8620
Lorenzo <tognesco@gmail.com>
2018-09-17 00:05:16 +0200

    Filtering products

--- routes/product.js
+++ routes/product.js
@@ -5,14 +5,51 @@ const { Category } = require('../models/categories');
 const { User } = require('../models/users');
 
 router.get('/', async (req, res) => {
-    const product = await Product.find();
-    res.send(product);
+    const category = await Product.find().select(
+        { _id: 0, "category._id": 0, 
+        "seller._id": 0, 
+        "seller.isAdmin": 0
+        });
+    res.send(category);
 });
 
+router.get('/nameCrescente', async (req, res) => {
+    const category = await Product.find().select(
+        { _id: 0, "category._id": 0, 
+        "seller._id": 0, 
+        "seller.isAdmin": 0
+        }).sort({ name: 1 });
+    res.send(category);
+});
 
+router.get('/nameDecrescente', async (req, res) => {
+    const category = await Product.find().select(
+        { _id: 0, "category._id": 0, 
+        "seller._id": 0, 
+        "seller.isAdmin": 0
+        }).sort({ name: -1 });
+    res.send(category);
+});
+
+router.get('/priceCrescente', async (req, res) => {
+    const category = await Product.find().select(
+        { _id: 0, "category._id": 0, 
+        "seller._id": 0, 
+        "seller.isAdmin": 0
+        }).sort({ price: 1 });
+    res.send(category);
+});
+
+router.get('/priceDecrescente', async (req, res) => {
+    const category = await Product.find().select(
+        { _id: 0, "category._id": 0, 
+        "seller._id": 0, 
+        "seller.isAdmin": 0
+        }).sort({ price: -1 });
+    res.send(category);
+});
 
 router.post('/', async(req, res) => {
-
     const category = await Category.findOne({ name: req.body.category}); //TODO IF ERROR
     
     const user = await User.findOne({ _id: req.body.id }); //TODO IF ERROR E PRENDERE JWT TOKEN INFO
