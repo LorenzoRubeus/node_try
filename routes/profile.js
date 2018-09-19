6e46e48322c8eab3ca2c4242366a4f60db50435e
Lorenzo <tognesco@gmail.com>
2018-09-19 23:06:44 +0200

    Updated view profileChangeEmail

--- routes/profile.js
+++ routes/profile.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const { User } = require('../models/users');
 const config = require('config');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const auth = require('../middleware/auth');
-const setHeaderToken = require('../middleware/setHeaderToken');
 
 router.get('/:token', async (req, res) => {
     const token = req.params.token;
@@ -39,6 +39,23 @@ router.post('/changeName/:token', async (req, res) => {
 
     const user = await User.findOneAndUpdate({ _id: decoded._id }, { firstName: req.body.txtFirstName, lastName: req.body.txtLastName }, { new: true });
     
+    res.render('profile', { user: user, token: token });
+});
+
+router.post('/changeEmail/:token', async (req, res) => {
+    const token = req.params.token;
+    const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
+
+    const hash = await bcrypt.genSalt(10);
+    const user = await User.findOneAndUpdate({ _id: decoded._id }, { token: token });
+
+    /*if(bcrypt.hash(req.body.txtPassword, hash) == user.password){
+        res.send('ciao');
+    }*/
+
+    const pass = await bcrypt.hash(req.body.txtPassword, hash);
+    return res.send(pass);
+
     res.render('profile', { user: user, token: token });
 });
 
