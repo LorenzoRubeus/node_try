e73ce620872fe7f4f4618bd57a9f85e907d2508b
Lorenzo <tognesco@libero.it>
2018-09-20 18:36:09 +0200

    View profilechangePassword created textboxes, created two routes for password in route profile.js

--- routes/profile.js
+++ routes/profile.js
@@ -28,10 +28,21 @@ router.get('/changeEmail/:token', async (req, res) => {
     const token = req.params.token;
     const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
 
-    const user = await User.findById(decoded._id).select({ isAdmin: 0, firstName: 0, lastName: 0, password: 0})
+    const user = await User.findById(decoded._id).select({ isAdmin: 0, password: 0})
 
     res.render('profileChangeEmail', { user: user, token: token });
 });
+
+router.get('/changePassword/:token', async (req, res) => {
+    const token = req.params.token;
+    const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
+
+    const user = await User.findById(decoded._id).select({ password: 0, isAdmin: 0 });
+
+    res.render('profileChangePassword', { user: user, token: token});
+});
+
+
 
 router.post('/changeName/:token', async (req, res) => {
     const token = req.params.token;
@@ -45,7 +56,6 @@ router.post('/changeName/:token', async (req, res) => {
 router.post('/changeEmail/:token', async (req, res) => {
     const token = req.params.token;
     const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
-    const hash = await bcrypt.genSalt(10);
 
     if(req.body.txtNewEmail !== req.body.txtConfirmNewEmail) { 
         return res.status(400).send('The two emails are not the same'); //TODO Da cambiare
@@ -61,6 +71,27 @@ router.post('/changeEmail/:token', async (req, res) => {
     await user.save();
 
     res.render('profile', { user: user, token: token });
+});
+
+router.post('/changePassword/:token', async (req, res) => {
+    const token = req.params.token;
+    const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
+    const hash = await bcrypt.genSalt(10);
+
+    const user = await User.findById(decoded._id);
+    const validPassword = await bcrypt.compare(req.body.txtNewPassword, user.password);
+    if( !validPassword ) {
+        return res.status(400).send('Your old password is wrong'); //TODO Da cambiare
+    }
+
+    if( req.body.txtNewPassword !== req.body.txtConfirmNewPassword ) {
+        return res.status(400).send('The passwords do not correspond'); //TODO Da cambiare
+    }
+    
+    user.password = await bcrypt.hash(req.body.txtNewPassword, hash);
+    await user.save();
+
+    res.render('profile', { user: user, token: token});
 });
 
 module.exports = router;
