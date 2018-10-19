cda729e20e41b6ed1b354232e3848e78047db1a3
Lorenzo <tognesco@libero.it>
2018-10-19 19:28:23 +0200

    Styled viewOrders and added route to filter orders by Month

--- routes/order.js
+++ routes/order.js
@@ -8,12 +8,65 @@ const btoa = require('btoa');
 
 router.get('/', auth, async (req, res) => {
     const orders = await Order.find({ customer: req.user._id }).populate('products');
+    let dateEstimated = [];
+    let dateOrder = [];
     let pictures = [];
-    for(let i = 0; i < orders.length; i++) {
-        pictures.push(getPictures(orders[i].products));
+    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+    for( let i = 0; i < orders.length; i++ ) {
+        dateOrder.push(getData(orders[i].dateOrder));
     }
-    res.render('viewOrders', { orders: orders, pictures: pictures });
+    for( let i = 0; i < orders.length; i++ ) {
+        dateEstimated.push(getData(orders[i].dateEstimated));
+    }
+    for( let i = 0; i < orders.length; i++ ) {
+        pictures.push(getPictures(orders[i].products));
+    } 
+
+    if( req.session.localVar ) {
+        let localVar = req.session.localVar;
+        req.session.destroy();
+        return res.render('viewOrders', { orders: localVar.orders, pictures: localVar.pictures, months: localVar.months, dateOrder: localVar.dateOrder, dateEstimated: localVar.dateEstimated });
+    }
+    res.render('viewOrders', { orders: orders, pictures: pictures, months: months, dateOrder: dateOrder, dateEstimated: dateEstimated });
 });
+
+router.get('/filterByMonth/:month', auth, async (req, res) => {
+    const monthFilter = req.params.month;
+    const orders = await Order.find({ customer: req.user._id }).populate('products');
+    let ordersFiltered = [];
+    let pictures = [];
+    let dateEstimated = [];
+    let dateOrder = [];
+    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+    for( let i = 0; i < orders.length; i++ ) {
+        let monthOrder = orders[i].dateOrder.getUTCMonth();
+        if( monthOrder == monthFilter) {
+            ordersFiltered.push(orders[i]);
+        }
+    }
+    for( let i = 0; i < ordersFiltered.length; i++ ) {
+        dateOrder.push(getData(ordersFiltered[i].dateOrder));
+    }
+    for( let i = 0; i < ordersFiltered.length; i++ ) {
+        dateEstimated.push(getData(ordersFiltered[i].dateEstimated));
+    }
+    for( let i = 0; i < ordersFiltered.length; i++ ) {
+        pictures.push(getPictures(ordersFiltered[i].products));
+    } 
+
+    req.session.localVar = {
+        orders: ordersFiltered,
+        pictures: pictures,
+        months: months,
+        dateOrder: dateOrder,
+        dateEstimated: dateEstimated
+    }
+
+    res.redirect('/api/orders');
+});
+
 
 
 function getPictures(products) {
@@ -24,5 +77,19 @@ function getPictures(products) {
     return pictures;
 }
 
+function getData(dateOrder) {
+    let date = dateOrder;
+    let month = date.getUTCMonth() + 1; //months from 1-12
+    let day = date.getUTCDate();
+    let year = date.getUTCFullYear();
+
+    let objDate = {
+        month: month,
+        day: day,
+        year: year
+    }
+
+    return objDate;
+}
 
 module.exports = router;
