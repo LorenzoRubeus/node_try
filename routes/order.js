35339a3f89fce4ca43b4bcf37a6c59fd6eb78319
Lorenzo <tognesco@libero.it>
2018-10-19 13:50:24 +0200

    Styling orders

--- routes/order.js
+++ routes/order.js
@@ -4,11 +4,25 @@ const { Order } = require('../models/orders');
 const auth = require('../middleware/auth');
 const express = require('express');
 const router = express.Router();
+const btoa = require('btoa');
 
 router.get('/', auth, async (req, res) => {
     const orders = await Order.find({ customer: req.user._id }).populate('products');
-    //res.send(orders[1].products);
-    res.render('viewOrders', { orders: orders });
+    let pictures = [];
+    for(let i = 0; i < orders.length; i++) {
+        pictures.push(getPictures(orders[i].products));
+    }
+    res.render('viewOrders', { orders: orders, pictures: pictures });
 });
+
+
+function getPictures(products) {
+    let pictures = [];
+    for(let i = 0; i < products.length; i++) {
+        pictures.push(btoa(products[i].img.data));
+    }
+    return pictures;
+}
+
 
 module.exports = router;
